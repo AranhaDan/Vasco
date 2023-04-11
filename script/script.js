418d3b619ef219c5f2743ec1d6b9101d5b7a181e
AranhaDan <daniel.macae_@hotmail.com>
2023-04-10 23:19:06 -0300

    .

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function clickMenu() {
+    if (hamenu.style.display == 'block') {
+        hamenu.style.display = 'none'
+    } else {hamenu.style.display = 'block'}
+    }
+
+function mudTama() {
+        if (window.innerWidth >= 769) {
+            hamenu.style.display = 'block'
+        } else {
+            hamenu.style.display = 'none'
+        }
+    }
